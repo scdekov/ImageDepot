11de3d3ea59c8bf2bbdb89e3bdff4d6a7ed9a3c0
Svetoslav Dekov <svetlio1994@gmail.com>
2019-01-31 20:12:31 +0200

    Require authorization for creating links

--- app/controllers/api/links_controller.rb
+++ app/controllers/api/links_controller.rb
@@ -1,5 +1,12 @@
 module Api
   class LinksController < ApplicationController
+    include ActionController::HttpAuthentication::Basic::ControllerMethods
+    include ActionController::HttpAuthentication::Token::ControllerMethods
+
+    TOKEN = Rails.application.credentials[:API_TOKEN]
+
+    before_action :authorize, except: [:show]
+
     def show
       path = Depot.get_img_path(show_link_params)
       !path.nil? ? send_file(path) : render(status: 404)
@@ -7,6 +14,14 @@ module Api
 
     def create
       render json: { links: Depot.get_links(create_link_params) }
+    end
+
+    private
+
+    def authorize
+      authenticate_or_request_with_http_token do |token, _|
+        ActiveSupport::SecurityUtils.secure_compare(token, TOKEN)
+      end
     end
 
     def create_link_params
